168656f93ebdd31cf023b4b0403ec6c0af9f0753
Filip Balucha <balucha.filip2@gmail.com>
2020-02-05 23:54:53 +0000

    Fix documentation

--- checkpoint_4/TrafficSimulation.py
+++ checkpoint_4/TrafficSimulation.py
@@ -46,10 +46,8 @@ class TrafficSimulation(object):
         """
         # print(f"Calculating avg. speed for {int(density*100)}%")
         print(f"Calculating avg. speed for {density*100:.2f}%")
+        arr = self._road_array(density)
         num_cars = int(density * self.size)
-        num_empty = self.size - num_cars
-        # create an array of integers where one represents a segment which has a car
-        arr = np.array([0] * num_empty + [1] * num_cars)
         # shuffle the array in place to simulate random distribution of cars in traffic
         np.random.shuffle(arr)
         avg_speed_last = -1
@@ -66,7 +64,7 @@ class TrafficSimulation(object):
         """Performs a single traffic step
 
         Arguments:
-            arr {[int]} -- an array of 1s and 0s where 1 represents car and 0 an empty road segment
+            arr {[int]} -- an array of 1s and 0s where 1 represents a car and 0 an empty road segment
 
         Returns:
             [int] -- the array after the traffic step
@@ -93,7 +91,21 @@ class TrafficSimulation(object):
                     new_arr[i] = 0
         return new_arr, num_moved
 
-    def print_road(self, cars):
+    def _road_array(self, density):
+        """Returns an array that represents the road using 1s and 0s where 1 represents a car and 0 an empty road segment
+
+        Arguments:
+            density {float} -- car density
+
+        Returns:
+            [int] -- a numpy array
+        """
+        num_cars = int(density * self.size)
+        num_empty = self.size - num_cars
+        # create an array of integers where one represents a segment which has a car
+        return np.array([0] * num_empty + [1] * num_cars)
+
+    def _print_road(self, cars):
         BAR = "---".join("|" * (len(cars)+1))
         CONTENT = "| " + \
             " | ".join(["C" if has_car else " " for has_car in cars]) + " |"
@@ -110,10 +122,8 @@ class TrafficSimulation(object):
             num_iterations {int} -- number of iterations
         """
 
-        num_cars = int(density * self.size)
-        num_empty = self.size - num_cars
-        arr = np.array([0] * num_empty + [1] * num_cars)
+        arr = self._road_array(density)
 
         for _ in range(num_iterations):
-            self.print_road(arr)
+            self._print_road(arr)
             arr, _ = self.traffic_step(arr)
