f938149c3e1dec63152657398e0b82f608e1da0e
Filip Balucha <balucha.filip2@gmail.com>
2020-03-12 07:35:43 +0000

    Implement test class

--- project/SimulationTest.py
+++ project/SimulationTest.py
@@ -1,3 +1,20 @@
+import Simulation
+
+FILE_NAME = "body_data.csv"
+TIMESTEP = 0.01
+NUM_ITERATIONS = 500
+
+
 class SimulationTest(object):
-    def __init__(self):
+
+    def __init__(self, file_name, timestep, num_iterations):
+        self.simulation = Simulation(file_name, timestep, num_iterations)
+
+    def run(self):
+        # self.simulation().animate()
         pass
+
+
+if __name__ == "__main__":
+    test = SimulationTest(FILE_NAME, TIMESTEP, NUM_ITERATIONS)
+    test.run()
